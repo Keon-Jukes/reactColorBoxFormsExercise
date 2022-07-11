cd761c1e56438050a8d9ecb0614c7205f73d6a98
keonjukes <keonofthephanerozoiceon@gmail.com>
2022-07-11 19:19:26 -0400

    value added to form input

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -4,22 +4,41 @@ class NewBoxForm extends Component {
     constructor(props){
         super(props)
         this.state = {
-            'box-color' : '',
-            'box-width' : '',
-            'box-height' : ''
+            'boxColor' : '',
+            'boxWidth' : '',
+            'boxHeight' : ''
         }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
     }
+
+    handleRemove(evt){
+
+    }
+
+
+    handleSubmit(evt){
+
+    }
+
+    handleChange(evt){
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+
     render(){
         return(
             <div>
-                <form>
-                    <label htmlFor='box-color'>Box Color: </label>
-                    <input name='box-color' id='box-color' placeholder='box color' />
-                    <label htmlFor='box-height'>Box Height: </label>
-                    <input name='box-height' id='box-height'  placeholder='box height'/>
-                    <label htmlFor='box-width'>Box Width: </label>
-                    <input name='box-width' id='box-width' placeholder='box width'/>
-                    <button>Create Box</button>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='boxColor'>Box Color: </label>
+                    <input name='box-color' id='boxColor' placeholder='box color' value={this.state.boxColor}/>
+                    <label htmlFor='boxHeight'>Box Height: </label>
+                    <input name='box-height' id='boxHeight'  placeholder='box height' value={this.state.boxHeight}/>
+                    <label htmlFor='boxWidth'>Box Width: </label>
+                    <input name='box-width' id='boxWidth' placeholder='box width' value={this.state.boxWidth}/>
+                    <button>Create Box!</button>
                 </form>
             </div>
         )
